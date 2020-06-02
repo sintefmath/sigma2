83ebdce7d867f40709f26c5d87197ac02905963b
Franz G. Fuchs <franz.fuchs@sintef.no>
2020-06-02 11:53:11 +0200

    added suitesparse as dependency

--- opm/modules_to_load.sh
+++ opm/modules_to_load.sh
@@ -3,6 +3,7 @@ module load GCCcore/8.2.0
 module load icc/2019.1.144-GCC-8.2.0-2.31.1
 module load impi/2018.4.274-iccifort-2019.1.144-GCC-8.2.0-2.31.1
 #module load Boost/1.71.0-GCC-8.3.0
+module load SuiteSparse/5.4.0-intel-2019a-METIS-5.1.0
 module load OpenBLAS/0.3.5-GCC-8.2.0-2.31.1
 module load  Python/3.7.2-GCCcore-8.2.0
 export MY_CC=gcc
