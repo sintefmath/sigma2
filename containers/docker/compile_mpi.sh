719cf96c8ba827b1c5e5688867ea50447236764f
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2020-06-05 18:40:05 +0200

    Fixes for MPICH compilation

--- containers/docker/compile_mpi.sh
+++ containers/docker/compile_mpi.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+export CFLAGS='-O2'
+export CXXFLAGS='-O2'
+
 if [ "${OPM_MPI_TYPE}" == "OPENMPI" ];
 then
   ##### OPENMPI
@@ -17,15 +20,14 @@ elif [ "${OPM_MPI_TYPE}" == "MPICH" ];
 then
   ##### MPICH
   cd ${SOURCE_CODE_DIR}
-  wget https://github.com/pmodels/mpich/archive/v3.1.4.tar.gz
-  DEBIAN_FRONTEND=noninteractive apt-get install -y libtool autoconf
-  tar xf v3.1.4.tar.gz
-  cd mpich-3.1.4
-  ./autogen.sh
+  git clone -b v3.3.2 --recursive https://github.com/pmodels/mpich
+  cd mpich
+  # see https://github.com/pmodels/mpich/issues/2643
+  PERL_USE_UNSAFE_INC=1 ./autogen.sh
   ./configure --prefix=$INSTALL_PREFIX --disable-fortran
   make install
   cd ${SOURCE_CODE_DIR}
-  rm -rf mpich-*;
+  rm -rf mpich;
 else
   >&2 echo "Unknown MPI type ${OPM_MPI_TYPE}".
   exti 1
--- containers/docker/run_simulator.sh
+++ containers/docker/run_simulator.sh
@@ -8,10 +8,25 @@ else
   number_of_processes=$1
 fi
 
-# Make sure MPI believes we have enough slots
-echo "localhost slots=$number_of_processes" > hostfile
+mpirun_version_output=$(mpirun --version)
+
+# MPICH and OpenMPI require different hostfiles...
+if [[ $mpirun_version_output == *"Open MPI"* ]];
+then
+  function run_mpi_with_hosts {
+    # Make sure MPI believes we have enough slots
+    echo "localhost slots=$number_of_processes" > hostfile
+    time mpirun --hostfile hostfile "$@"
+  }
+else
+  function run_mpi_with_hosts {
+    echo "localhost:$number_of_processes" > hosts
+    mpirun -f hosts "$@"
+  }
+fi
+
 
 # Run simulator
-time mpirun --hostfile hostfile -np $number_of_processes $SOURCE_CODE_DIR/opm-simulators/build/bin/flow \
+run_mpi_with_hosts -np $number_of_processes $SOURCE_CODE_DIR/opm-simulators/build/bin/flow \
   $DATA_DIR/opm-data/norne/NORNE_ATW2013.DATA \
   --threads-per-process=1 --output-dir=output
